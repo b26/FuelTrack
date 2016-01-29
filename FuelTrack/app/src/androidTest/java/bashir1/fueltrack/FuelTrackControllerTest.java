7c7403543427a7b5c5079f79893afbeef241b069
Bashir Osman <bashir@b26.co>
2016-01-29 11:03:18 -0700

    added FuelTrackControllerTest

---
+++ FuelTrack/app/src/androidTest/java/bashir1/fueltrack/FuelTrackControllerTest.java
@@ -0,0 +1,26 @@
+package bashir1.fueltrack;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by X on 16-01-29.
+ */
+public class FuelTrackControllerTest extends ActivityInstrumentationTestCase2 {
+
+    public FuelTrackControllerTest() {
+        super(MainActivity.class);
+    }
+
+
+    public void testGetEntry() {
+        assertFalse(true);
+    }
+
+    public void testGetLogs() {
+        assertFalse(true);
+    }
+
+    public void testSort() {
+        assertFalse(true);
+    }
+}
