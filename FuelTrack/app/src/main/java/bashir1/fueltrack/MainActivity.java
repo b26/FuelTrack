83d5148d4b2d54743ab2c017e9d09e21a13bc666
Bashir Osman <bashir@b26.co>
2016-01-30 21:53:06 -0700

    removed the activity settings button from mainActivity

--- FuelTrack/app/src/main/java/bashir1/fueltrack/MainActivity.java
+++ FuelTrack/app/src/main/java/bashir1/fueltrack/MainActivity.java
@@ -82,18 +82,4 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/U®p button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
